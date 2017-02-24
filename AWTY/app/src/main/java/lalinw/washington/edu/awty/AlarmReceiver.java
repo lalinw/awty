0c3947f6bffe8d814317acb9385e7d1dc83e9864
lalinw <i.lalinw@gmail.com>
2017-02-24 04:08:56 -0800

    works now :)

--- AWTY/app/src/main/java/lalinw/washington/edu/awty/AlarmReceiver.java
+++ AWTY/app/src/main/java/lalinw/washington/edu/awty/AlarmReceiver.java
@@ -3,6 +3,7 @@ package lalinw.washington.edu.awty;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -16,7 +17,11 @@ public class AlarmReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
 
         // For our recurring task, we'll just display a message
-        Log.i("onRECEIVE", "run run run");
-        Toast.makeText(context, "AWTY is running nowwww", Toast.LENGTH_SHORT).show();
+        String phone = intent.getStringExtra("phone");
+        String msg = intent.getStringExtra("message");
+
+        String phoneStyled = "(" + phone.substring(0, 3) + ") " + phone.substring(3, 6) + "-" + phone.substring(6);
+        String phn_msg = phoneStyled + ": " + msg;
+        Toast.makeText(context, phn_msg, Toast.LENGTH_SHORT).show();
     }
 }
--- AWTY/app/src/main/java/lalinw/washington/edu/awty/MainActivity.java
+++ AWTY/app/src/main/java/lalinw/washington/edu/awty/MainActivity.java
@@ -3,9 +3,11 @@ package lalinw.washington.edu.awty;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.SystemClock;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,58 +18,84 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     public PendingIntent pendingIntent;
+    public Intent alarmIntent;
     private static boolean start = false;
-    public AlarmManager alarmManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final Button stst = (Button) findViewById(R.id.start_stop);
+
+        if (start) {
+            stst.setText("Stop");
+            stst.setBackgroundColor(Color.parseColor("#e60000"));
+        } else {
+            stst.setText("Start");
+            stst.setBackgroundColor(Color.parseColor("#a4c639"));
+        }
         //red hex #e60000
         //green hex #a4c639
+
 
         final EditText et_msg = (EditText) findViewById(R.id.message);
         final EditText et_phn = (EditText) findViewById(R.id.phone);
         final EditText et_frq = (EditText) findViewById(R.id.time);
 
-        /* Retrieve a PendingIntent that will perform a broadcast */
-        Intent alarmIntent = new Intent(MainActivity.this, AlarmReceiver.class);
-        pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, alarmIntent, 0);
-
-        final Button stst = (Button) findViewById(R.id.start_stop);
         stst.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                /* Retrieve a PendingIntent that will perform a broadcast */
+                alarmIntent = new Intent(getApplicationContext(), AlarmReceiver.class);
+                pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, alarmIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
                 String msg = et_msg.getText().toString();
                 String phn = et_phn.getText().toString();
                 String frq_str = et_frq.getText().toString();
                 int frq = Integer.parseInt(frq_str);
 
-                if (start) {
-                    //alarm manager is running, stop the service
-                    start = false;
-                    stst.setText("Start");
-                    stst.setBackgroundColor(Color.parseColor("#a4c639"));
-                    cancel();
+                if (msg.length() > 0 && phn.length() > 0 && frq > 0) {
+                    if (start) {
+                        //alarm manager is running, stop the service
+                        start = false;
+                        stst.setText("Start");
+                        stst.setBackgroundColor(Color.parseColor("#a4c639"));
+                        cancel();
+                    } else {
+                        //starts the alarm manager
+                        alarmIntent.putExtra("phone", phn);
+                        alarmIntent.putExtra("message", msg);
+                        pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, alarmIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                        start = true;
+                        stst.setText("Stop");
+                        stst.setBackgroundColor(Color.parseColor("#e60000"));
+                        start(frq);
+                    }
+
                 } else {
-                    //starts the alarm manager
-                    start = true;
-                    stst.setText("Stop");
-                    stst.setBackgroundColor(Color.parseColor("#e60000"));
-                    start(msg, phn, frq);
+                    AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                    alertDialog.setTitle("Attention!");
+                    alertDialog.setMessage("Please fill in all fields with valid values.");
+                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    alertDialog.show();
                 }
+
+
             }
         });
 
     }
 
-    public void start(String message, String phone, int minutes) {
+    public void start(int minutes) {
         AlarmManager manager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-        int interval = 1000 * 2 * minutes; //time in milliseconds
-        String phoneStyled = "(" + phone.substring(0, 3) + ") " + phone.substring(3, 6) + "-" + phone.substring(6);
-
-        manager.setRepeating(AlarmManager.RTC_WAKEUP, SystemClock.elapsedRealtime(), interval, pendingIntent);
-        Toast.makeText(this, phoneStyled + ": " + message, Toast.LENGTH_SHORT).show();
+        int interval = 1000 * 60 * minutes; //time in milliseconds
+        manager.setInexactRepeating(AlarmManager.RTC_WAKEUP, SystemClock.elapsedRealtime(), interval, pendingIntent);
+        Toast.makeText(this, "AWTY started", Toast.LENGTH_SHORT).show();
     }
 
     public void cancel() {
